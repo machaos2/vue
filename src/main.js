22771da45abbf6b20c007a8f1f5d4dcabe76979b
井梧故园秋 <beherochuling@163.com>
2018-07-21 00:04:54 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App'
+import router from './router'
+import './directives'
+import './components'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  router,
+  components: { App },
+  template: '<App/>'
+});

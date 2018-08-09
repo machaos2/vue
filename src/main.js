f4349180bd44173d91b5b46b2c7cdad9fff86b62
beesoft <beherochuling@163.com>
2018-08-09 10:12:23 +0800

    bee

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import './directives'
-import './components'
+import './directives';
+import './components';
 
 import $ from 'jquery';
 import 'chosen-js/chosen.css';
--- src/views/tools/AssociationalWord.vue
+++ src/views/tools/AssociationalWord.vue
@@ -30,29 +30,28 @@
         </div>
       </div>
     </div>
-    <!-- <div class="col-md-8">
-      <el-tabs v-model="activeName" @tab-click="handleClick" v-if="results">
+    <div class="col-md-8">
+      <el-tabs v-model="activeName" v-if="results">
         <el-tab-pane :label="result.title" :name="result.slug" v-for="result in results" :key="result.slug">
-          <el-table :data="result.data" height="750" border style="width: 100%;" :span-method="objectSpanMethod">
+          <!-- <el-table :data="result.data" height="750" border style="width: 100%;" :span-method="objectSpanMethod">
             <el-table-column prop="title" label="关键词" width="180" rowspan="associated.length"></el-table-column>
             <el-table-column prop="item" label="联想词" width="180" v-for="item in associated"></el-table-column>
-          </el-table>
+          </el-table> -->
         </el-tab-pane>
       </el-tabs>
-    </div> -->
+    </div>
 
   </div>
 </template>
 
 <script>
   import ls from '@/utils/localStorage';
+  import associated from '@/utils/associted.js';
   import VueChosen from '@/components/vendor/vue-chosen';
 
   window.sogou = {};
   sogou.sug = function(res) {
     res = JSON.parse(JSON.stringify(res));
-    // console.log(res);
-    // console.log(res[1]);
     for ( let i of res[1] ) {
       console.log(i);
     }
@@ -77,7 +76,7 @@
 阜阳`,
         append: '整形',
         search: '壹加壹',
-        engine_selected: ['baidu_wap', 'sogou_wap'],
+        engine_selected: ['baidu_web'],
         placeholder: "请选择",
         engines: [
           {
@@ -190,236 +189,11 @@
             let items = this.words.split('\n');
 
             items.forEach(item => {
-              let temp = item+this.append;
-
-              switch ( engine ) {
-                case 'baidu_wap':
-                    $.ajax({
-                        url : "http://m.baidu.com/su",
-                        async: false,
-                        crossDomain: true,
-                        dataType: 'jsonp',
-                        data : {
-                            pre: 1,
-                            p: 3,
-                            ie: 'utf-8',
-                            json: 1,
-                            from: 'wise_web',
-                            sugsid: '124615,125148,122155,117037,123799,124817,120133,125141,118885,118874,118848,118830,118805,107313,125020,125006,124978,117431,124603,122788,125085,124752,124634,124748,123985,124559,124110,124708,124525,122496,124770,124030,110085,123290,124425,125011',
-                            net: '',
-                            os: '',
-                            sp: '',
-                            wd: temp,
-                            _: 1533632156265,
-                        },
-                        success(res){
-                            // console.log(res);
-                            // console.log(res.s);
-                            for ( let i of res.s ) {
-                              console.log(i);
-                            }
-                        },
-                    });
-                break;
-                case 'baidu_web':
-                    $.ajax({
-                        url : "https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su",
-                        async: false,
-                        crossDomain: true,
-                        dataType: 'jsonp',
-                        jsonp: 'cb',
-                        data : {
-                            wd: temp,
-                            sugmode: 2,
-                            json: 1,
-                            p: 3,
-                            sid: '1439_26965_21109_26350_26920_20930',
-                            req: 2,
-                            bs: temp,
-                            pbs: temp,
-                            csor: 5,
-                            pwd: temp,
-                            // cb: "",
-                            _: 1533630813225,
-                        },
-                        success(res){
-                            // console.log(res);
-                            // console.log(res.s);
-                            for ( let i of res.s ) {
-                              console.log(i);
-                            }
-                        },
-                    });
-                break;
-                case 'sogou_wap':
-                    $.ajax({
-                        url : "https://wap.sogou.com/web/sugg.jsp",
-                        async: false,
-                        crossDomain: true,
-                        dataType: 'jsonp',
-                        jsonp: 'cb',
-                        data : {
-                            vr: 1,
-                            kw: temp,
-                            t: 1533633449734,
-                            prereq_a: '',
-                            sugsuv: 'AAEWaUrKIQAAAAqUCyFx3AAAkwA=',
-                            suguuid: '16cb663b-7016-43bb-8ac5-afb99e8833f9',
-                            sugtime: 1533633449734,
-                        },
-                        success(res){
-                            // console.log(res);
-                            // console.log(res.s);
-                            for ( let i of res.s ) {
-                              console.log(i);
-                            }
-                        },
-                        // error(a, b, c){
-                        //   console.log(a);
-                        //   console.log(b);
-                        //   console.log(c);
-                        // },
-                    });
-                break;
-                case 'sogou_web':
-                    $.ajax({
-                        url : "https://www.sogou.com/suggnew/ajajjson",
-                        async: false,
-                        crossDomain: true,
-                        dataType: 'jsonp',
-                        jsonp: 'cb',
-                        data : {
-                            key: temp,
-                            type: 'web',
-                            ori: 'yes',
-                            pr: 'web',
-                            abtestid: 0,
-                            ipn: '',
-                            t: 1533632510344,
-                            suguuid: '6fb52a51-02fa-4565-9fa8-a9603b53a01c',
-                            ip: '36.7.137.10',
-                            iploc: 3401,
-                            suid: '0A8907242013940A000000005B617D5D',
-                            yyid: 'null',
-                            pid: 'sogou',
-                            policyno: 'null',
-                            mfp: 'null',
-                            hs: 'https',
-                            mp: 1,
-                            prereq_a: '',
-                            sugsuv: 1533115739339469,
-                            sugtime: 1533632523492,
-                        },
-                        success(res){
-                            console.log(res);
-                            console.log(res[1]);
-                            for ( let i of res[1] ) {
-                              console.log(i);
-                            }
-                        },
-                    });
-                break;
-                case '360_web':
-                    $.ajax({
-                        url : "https://sug.so.360.cn/suggest",
-                        async: false,
-                        crossDomain: true,
-                        dataType: 'jsonp',
-                        data : {
-                            encodein: 'utf-8',
-                            encodeout: 'utf-8',
-                            format: 'json',
-                            fields: 'word',
-                            word: temp,
-                            // _: 1533636209727,
-                        },
-                        success(res){
-                            console.log(res);
-                            console.log(res.result);
-                            for ( let i of res.result ) {
-                              console.log(i.word);
-                            }
-                        },
-                    });
-                break;
-                case '360_wap':
-                    $.ajax({
-                        url : "https://m.so.com/suggest/mso",
-                        async: false,
-                        crossDomain: true,
-                        dataType: 'json',
-                        data : {
-                            kw: temp,
-                            src: 'mso',
-                            caller: 'strict',
-                            sensitive: 'strict',
-                            count: 10,
-                        },
-                        success(res){
-                            // console.log(res);
-                            // console.log(res.data.sug);
-                            if ( res.data.sug ) {
-                              for ( let i of res.data.sug ) {
-                                console.log(i.word);
-                              }
-                            }
-                        },
-                    });
-                break;
-                case 'shenma':
-                    $.ajax({
-                        url : "https://sugs.m.sm.cn/web",
-                        async: false,
-                        crossDomain: true,
-                        dataType: 'jsonp',
-                        data : {
-                            scheme : "https",
-                            fr : "android",
-                            t : "w",
-                            uc_param_str : "dnnwnt",
-                            q : temp,
-                            // callback: '',
-                            _ : "1533178782582",
-                        },
-                        success(res){
-                            // console.log(res);
-                            // console.log(res.r);
-                            for ( let i of res.r ) {
-                              console.log(i.w);
-                            }
-                        },
-                    });
-                break;
-              }
-              console.log(this.search);
+              let result = associated(engine, item+this.append);
+              // console.log(result);
             });
           })
-          // const target = e.target.type === 'submit' ? e.target : e.target.parentElement;
-
-          // if (target.canSubmit) this.submit();
         });
-      },
-      objectSpanMethod({ row, column, rowIndex, columnIndex }) {
-        console.log(row);
-        console.log(column);
-        console.log(rowIndex);
-        console.log(columnIndex);
-        if (columnIndex === 0) {
-          if (rowIndex % 2 === 0) {
-            return {
-              rowspan: 2,
-              colspan: 1
-            };
-          } else {
-            return {
-              rowspan: 0,
-              colspan: 0
-            };
-          }
-        }
-      },
-      handleClick(tab, event) {
-        console.log(tab, event);
       },
       setSelectedEngines(selected) {
         this.engine_selected = selected;
